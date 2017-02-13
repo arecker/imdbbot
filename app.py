cd498258a3a14b230399fdd4faac320bf4b86e99
Alex Recker <alex@reckerfamily.com>
2017-02-12 19:48:43 -0600

    Tweak

--- app.py
+++ app.py
@@ -7,16 +7,31 @@ app = Chalice(app_name='imdbbot')
 imdb = IMDb()
 
 
+def to_response(text, public=True):
+    response = {
+        'text': text,
+        'unfurl_links': True
+    }
+
+    if public:
+        response['response_type'] = 'in_channel'
+
+    return response
+
+
 @app.route('/', cors=True)
 def handler():
     title = app.current_request.query_params.get('text', None)
 
     if not title:
-        return 'Need a movie title, stupid'
+        return to_response('Need a movie title, stupid', public=False)
 
     results = imdb.search_movie(title)
 
     if results:
-        return 'http://www.imdb.com/title/tt' + results[0].getID()
+        return to_response('http://www.imdb.com/title/tt' + results[0].getID())
 
-    return 'Sorry - couldn\'t find "{}" on imdb.'.format(title)
+    return to_response(
+        'Sorry - couldn\'t find "{}" on imdb.'.format(title),
+        public=False
+    )
